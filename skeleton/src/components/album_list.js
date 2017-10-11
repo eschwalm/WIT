cfed5beb22c08b4dcc04386e257369e05f53f4a1
PriyaG88 <priya.g71488@gmail.com>
2017-10-11 10:45:35 -0700

    Add skeleton file for components

---
+++ skeleton/src/components/album_list.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import Axios from 'axios';
+import AlbumDetail from './album_detail';
+
+class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { albums: [] };
+  }
+  componentWillMount() {
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
+    );
+  }
+}
+
+export default AlbumList;
