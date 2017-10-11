3110f778dd79374dd4edab1e8ea5d3c968772afa
Evan <schwalm.evan@gmail.com>
2017-10-10 21:39:30 -0700

    add posts index and delete

--- backend/app/routes/post_routes.js
+++ backend/app/routes/post_routes.js
@@ -1,6 +1,7 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
+  //CREATE
   app.post('/posts', (req, res) => {
     const post = {
       img: req.body.img,
@@ -18,6 +19,12 @@ module.exports = function(app, db) {
     });
   });
 
+  //INDEX
+  app.get('/posts', (req, res) => {
+    db.collection('posts').find();
+  });
+
+  //SHOW
   app.get('/posts/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
@@ -26,6 +33,19 @@ module.exports = function(app, db) {
         res.send({'error':'An error has occurred'});
       } else {
         res.send(item);
+      }
+    });
+  });
+
+  //DELETE
+  app.delete('/posts/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('posts').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Post ' + id + ' deleted!');
       }
     });
   });
