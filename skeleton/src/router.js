bd0bffce5b85b934d55e785395221e59be3ea4bd
PriyaG88 <priya.g71488@gmail.com>
2017-10-13 10:32:28 -0700

    Add new scene for creating posts

--- skeleton/src/router.js
+++ skeleton/src/router.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import PostsIndexContainer from './containers/posts_index_container';
+import NewPostForm from './components/new_post_form';
 import PostView from './components/post_view';
 
 const Routing = () => {
   return (
     <Router>
-      <Scene key="root" >
-        <Scene key='postIndex' component={PostsIndexContainer} title='Posts' />
+      <Scene key="main" >
+        <Scene key='postIndex'
+          onRight={() => Actions.postForm()}
+          rightTitle='Add'
+          component={PostsIndexContainer}
+          title='Posts' />
+        <Scene key='postForm'
+          component={NewPostForm}
+          title='createPost'/>
       </Scene>
     </Router>
   );
