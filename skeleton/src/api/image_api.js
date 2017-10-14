1e537db765d23854300a2463d3087907682316c8
Evan <schwalm.evan@gmail.com>
2017-10-14 02:11:53 -0700

    edit image upload size

--- skeleton/src/api/image_api.js
+++ skeleton/src/api/image_api.js
@@ -1,11 +1,11 @@
+import cl from './config/cloudinary.js';
 import CryptoJS from 'crypto-js';
-import config from './config/cloudinary.js';
 
 export const uploadImage = (uri) => {
   let timestamp = (Date.now() / 1000 | 0).toString();
-  let key = config.api_key;
-  let secret = config.api_secret;
-  let cloud = config.cloud_name;
+  let key = cl.api_key;
+  let secret = cl.api_secret;
+  let cloud = cl.cloud_name;
   let hash = 'timestamp=' + timestamp + secret;
   let signature = CryptoJS.SHA1(hash).toString();
   let url = 'https://api.cloudinary.com/v1_1/' + cloud + '/image/upload/';
@@ -20,9 +20,8 @@ export const uploadImage = (uri) => {
   formdata.append('upload_preset', 'wit-app');
   formdata.append('timestamp', timestamp);
   formdata.append('api_key', key);
-  formdata.append('signature', signature);
+  // formdata.append('signature', signature);
   xhr.send(formdata);
 };
-
 // credits:
 // https://gist.github.com/cmcewen/b1ea2464aa88ae32be17
